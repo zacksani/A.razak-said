3f04f34f9868ea36fb31f023c4a4463b92d5aa20
zacksani <abdulrazakzakaria81@gmail.com>
2025-01-17 08:18:51 +0200

    g

--- document.getElementById('basic-price').js
+++
@@ -1,3 +0,0 @@
-document.getElementById('basic-price').innerText = '$99';
-document.getElementById('essential-price').innerText = '$199';
-document.getElementById('executive-price').innerText = '$299';
